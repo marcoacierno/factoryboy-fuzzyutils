0a860feb7a11e6df4f5fbff1e188e113a4ae7603
Marco Acierno <marcoacierno96@gmail.com>
2016-05-16 23:21:02 +0200

    Change folder structure to release the package to pypi

---
+++ setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+
+
+setup(
+    name='fuzzy_utils',
+    version='1.1',
+    keywords=['factory_boy', 'fuzzy', 'utils'],
+    description='Additional fuzzy classes for Factory boy',
+    author='Marco Acierno',
+    author_email='marcoaciernoemail@gmail.com',
+    url='https://github.com/marcoacierno/factoryboy-fuzzyutils',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+
+        'Intended Audience :: Developers',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+
+        'Operating System :: OS Independent',
+
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
